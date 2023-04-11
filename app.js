de970e32c30ef3ddcfb21b3977152064239d4ce9
Samuel Adelowo <samueladelowo92@gmail.com>
2023-04-11 23:52:15 +0100

    add the deleted action

--- app.js
+++ app.js
@@ -75,6 +75,18 @@ app.post("/", function (req, res) {
   // }
 });
 
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndDelete(checkedItemId)
+    .then(() => {
+      console.log("SUccessfully deleted");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/");
+});
+
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
